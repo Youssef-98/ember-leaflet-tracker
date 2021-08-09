8517625ca2f80d0a3c3c890b2514990138fe1ac3
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-08-09 13:58:12 +0200

    added map

--- app/components/map.js
+++ app/components/map.js
@@ -1,3 +1,7 @@
 import Component from '@glimmer/component';
 
-export default class MapComponent extends Component {}
+export default class MapComponent extends Component {
+  lat = 51.0109;
+  lng = 3.7265;
+  zoom = 9;
+}
