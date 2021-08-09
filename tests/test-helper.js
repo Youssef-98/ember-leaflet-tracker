76bc267a665e702b8dc58807413a2436f17d1625
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-08-09 12:19:17 +0200

    Initial commit. Added first header and paragraph

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-leaflet-tracker/app';
+import config from 'ember-leaflet-tracker/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
