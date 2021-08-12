237e49fb22aece9259c8c9c482bf4ea497f5ef4b
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-08-12 13:31:22 +0200

    Shows polygon correctly for only one location

--- app/components/map.js
+++ app/components/map.js
@@ -6,28 +6,34 @@ import { start } from '../helpers/sparql';
 
 export default class MapComponent extends Component {
   @tracked municipalities = A([]);
+  @tracked coordinates = A([]);
 
   lat = 51.0109;
   lng = 3.7265;
   zoom = 9;
 
-  stadspark = [51.213522, 4.415624];
+  restaurants = [
+    [2.989816683000072, 50.91617313800003],
+    [2.9505748000000267, 50.87572132200006],
+    [2.8438445630000615, 50.91933953100005],
+  ];
 
   get url() {
     return 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   }
 
   get locations() {
-    return this.municipalities[0].coordinates;
+    return this.coordinates[0].map((c) => ({ lat: c[1], lng: c[0] }));
   }
 
   @action
   async start() {
     const result = await start();
+    console.log(result);
     this.municipalities = result;
-    /* this.municipalities.map((m, i) => {
-      console.log(`${i}: ${m.coordinates}`);
-    }); */
-    console.log(this.municipalities);
+    result.map((r, i) => {
+      i === 2 && (this.coordinates = r.coordinates);
+    });
+    console.log(this.coordinates);
   }
 }
