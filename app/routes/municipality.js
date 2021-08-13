efdf55d5328c1b20417d72b895f365df8c9629a9
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2021-08-13 17:02:28 +0200

    cleanup

--- app/routes/municipality.js
+++ app/routes/municipality.js
@@ -1,10 +1,9 @@
 import Route from '@ember/routing/route';
-import { fetchMunicipality, fetchDecisions } from '../helpers/sparql';
+import { fetchMunicipality } from '../helpers/sparql';
 
 export default class MunicipalityRoute extends Route {
   async model(params) {
     const municipality = await fetchMunicipality(params.cityName);
-    console.log(municipality);
     return municipality;
   }
 }
